63b1f3b2bdd27fd99b4e05527c74728113a517da
Michael James White <mike.james.white@icloud.com>
2018-05-19 15:39:05 +0200

    Implemented FileIO.

--- src/Person.java
+++ src/Person.java
@@ -6,6 +6,32 @@
  * Date: 19/05/2018
  */
 
+import java.util.*;
+
 public class Person {
-    
+    public int id;
+    public Queue<int[]> workPattern;
+
+    // parse an input file string and attain attributes for new Person object
+    public Person(String s) {
+        s = s.trim();
+        
+        // get ID
+        int begin = s.indexOf(' ');
+        id = Integer.parseInt(s.substring(0, s.indexOf(' ')));
+
+        // discard start of string, remove brackets, and split into an array
+        String [] split = s.substring(begin + 1).replace("(", "").replace(")", "").split(", ");
+
+        // create workPattern array
+        workPattern = new ArrayDeque<int[]>();
+        for (int i = 0; i < split.length/2; i++) {
+            workPattern.add( new int[] {Integer.parseInt(split[i*2]), Integer.parseInt(split[(i*2) + 1]) });
+        }
+    }
+
+    // get to required branch, work until done and work again if another job in queue, else terminate thread
+    public void work() {
+
+    }
 }
--- src/Simulator.java
+++ src/Simulator.java
@@ -6,9 +6,33 @@
  * Date: 19/05/2018
  */
 
+import java.io.*;
+import java.util.*;
+
 public class Simulator {
 
     public static void main (String[] args) {
+        int numPeople = 0;
+        int numBranches = 0;
+        Person[] people = null;
 
+        // parse input file
+        try {
+            File f = new File(args[0]);
+            Scanner in = new Scanner(f);
+
+            numPeople = Integer.parseInt(in.nextLine());
+            numBranches = Integer.parseInt(in.nextLine());
+            people = new Person[numPeople];
+            for (int i = 0; i < numPeople; i++) {
+                people[i] = new Person(in.nextLine());
+            }
+
+            in.close();
+        }
+        catch (Exception e) { e.printStackTrace(); return; }
+
+        // run simulation
+        
     }
 }
